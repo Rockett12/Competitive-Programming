ac2aba3cdcd87a742eeb5dfd623f0116b6e1cf61
Rockett <lipegold@hotmail.com>
2018-02-24 02:23:36 -0300

    gapa round20

--- OJ_Solutions/URI/2406.cpp
+++ OJ_Solutions/URI/2406.cpp
@@ -1,16 +1,13 @@
 #include <stack>
 #include <iostream>
 #define $ ios::sync_with_stdio(0);
-
 using namespace std;
-
 int main()
 {$
     int t, f;
     stack<char> st;
     string a;
     char x;
-
     cin>>t;
     while(t--)
     {
@@ -19,32 +16,20 @@ int main()
         while(!st.empty())
             st.pop();
 
-        for(int i =0; i<(int)a.size(); ++i)
-        {
-            x = a[i];
-            if(x=='{' || x == '[' || x == '(')
+        for(int i =0; i<(int)a.size() && f; ++i)
+            if((x = a[i])=='{' || x == '[' || x == '(')
                st.push(x);
             else if(!st.empty())
                 if(x==')' && st.top()=='(' || x==']' && st.top()=='[' || x=='}' && st.top()=='{')
                     st.pop();
                 else
-                {
                     f = 0;
-                    break;
-                }
             else
-            {
-                f=0;
-                break;
-            }
-
-        }
+                f = 0;
 
         if(st.empty() && f)
             cout<<"S\n";
         else
             cout<<"N\n";
     }
-
 }
-
