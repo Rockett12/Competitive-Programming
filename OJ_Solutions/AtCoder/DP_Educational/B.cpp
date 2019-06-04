72109afa5be9ff4644480900853e7803108aee18
Felipe <lipegold@hotmail.com>
2019-06-04 00:57:35 -0300

    fix

--- OJ_Solutions/AtCoder/DP_Educational/B.cpp
+++ OJ_Solutions/AtCoder/DP_Educational/B.cpp
@@ -1,50 +1,30 @@
 #include <bits/stdc++.h>
-
+#define F first
+#define S second
+#define $ ios::sync_with_stdio(0);
+#define endl '\n'
 using namespace std;
 using ll = long long int;
-using ii = tuple<int, int, int>;
-using li = pair<ll, int>;
+using ii = pair<int, int>;
 using vi = vector<int>;
 using vii = vector<ii>;
 using graph = vector<vi>;
-
-const ll mod =  1000000007;
 const int INF = 0x3f3f3f3f;
-const int MAXN = 2123456;
-const double pi = 3.14159265359;
-const double eps = 0.000000001;
-int fq[10];
-
+const ll INFL = 0x3f3f3f3f3f3f3f3f;
+const int MAXN = 112345;
+int dp[MAXN], h[MAXN];
 int main() {
-    int n, m, k;
-    scanf("%d", &n);
-    for (int i = 0, x; i<n; ++i) {
-        scanf("%d", &x);
-        fq[x]++;
+    int n, k;
+    scanf("%d %d", &n, &k);
+    for (int i = 1; i<=n; ++i) {
+        scanf("%d", h+i);
+        dp[i] = INF;
     }
-
-    if (fq[1] <= 1) {
-        if (fq[1] == 1) {
-            printf("1 ");
-        }
-        while (fq[2]--) {
-            printf("2 ");
-        }
-    } else {
-        printf("1 "); 
-        fq[1]--;
-        int k = fq[1] >> 1, q = fq[1]&1;
-        while (k--) {
-            printf("1 1 ");
-        }
-
-        while (fq[2]--) {
-            printf("2 ");
-        }
-        if (q) {
-            printf("1");
+    dp[1] = 0;
+    for (int i = 1; i<=n; ++i) {
+        for (int j = 1; j<=k && i+j<=n; ++j) {
+            dp[i+j] = min(dp[i+j], dp[i]+abs(h[i]-h[i+j]));
         }
     }
-    puts("");
+    printf("%d\n", dp[n]);
 }
-
